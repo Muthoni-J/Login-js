c1ab54f43416d69abc4c44e8781134a07c36c7cf
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-08-31 08:21:04 +0300

    first commit

---
+++ src/login.js
@@ -0,0 +1,53 @@
+import React from "react";
+import './login.css'
+import cyclist from './cyclist.jpg'
+function Login(){
+
+
+return(
+   <div className="holder">
+    <div className="Eddie">
+    <p><span>EddieShop</span></p>
+    <p className="SignUp">Don't have an account ?<span>SignUp</span></p>
+    </div>
+  
+   <h1>Welcome</h1>
+   <div className="details">
+    <form>
+        <div className="inputs">
+            <label className="email">Your Email
+            <input type = "email" name="emailAddress" placeholder="name@domain.com"/>
+            </label>
+        </div>
+        <div className="inputs">
+            <label >Password
+            <input type = "password" name="password" placeholder="at least 8 characters"/>
+            </label>
+        </div>
+        <div className="checkbox">
+            <label > 
+               <input type = "checkbox" name="text"/>
+               Keep me logged in
+          </label>
+            <label className="forgot">ForgotPassword?</label>
+        </div>
+        <div > 
+            <button type ="login">Login</button>
+        </div>
+        <img src= {cyclist} alt="cycling gal" className="gal"/>
+        <p>_____________________or_______________________</p>
+        
+        <div  className="social-icons">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-google"></i>
+            <i class="fa-brands fa-instagram"></i>
+
+        </div>
+        
+    </form>
+
+   </div>
+   </div> 
+)
+}
+export default Login
